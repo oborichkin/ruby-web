fa14a57703b6c98249a464911f1fa88e65e5a14e
pavel <oborin.p@gmail.com>
2020-05-14 02:36:25 +0300

    🇷🇺 Added localization

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,14 @@ Bundler.require(*Rails.groups)
 
 module MyProject
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.fallbacks = true
+    config.i18n.fallbacks = [:en]
+
+    config.i18n.enforce_available_locales = true
+
   end
 end
