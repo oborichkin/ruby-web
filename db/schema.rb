baddfe520705099b66debe3564f54f981fa04d40
pavel <oborin.p@gmail.com>
2020-05-20 00:45:56 +0300

    🗃 Users email unique migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_213511) do
+ActiveRecord::Schema.define(version: 2020_05_19_214508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_213511) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_token"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
